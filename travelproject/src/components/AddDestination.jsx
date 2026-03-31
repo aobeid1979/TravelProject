933a4284b0458de551efb8396ceb7211614e3245
aobeid1979 <amfo250@gmail.com>
2026-03-31 11:00:05 +0200

    Deleted the unused package.json file

--- travelproject/src/components/AddDestination.jsx
+++ travelproject/src/components/AddDestination.jsx
@@ -1,7 +1,20 @@
+import { useState } from "react";
+
 function AddDestination() {
+  const [city, setCity] = useState("");
+  const [country, setCountry] = useState("");
+
+  const handleAddDestination = (e) => {
+    e.preventDefault();
+    console.log("City: ", city);
+    console.log("Country: ", country);
+
+    setCity("");
+    setCountry("");
+  };
   return (
     <div className="py-4 border">
-      <form>
+      <form onSubmit={handleAddDestination}>
         <div className="row col-10 offset-1">
           <h4>Enter a new Destination</h4>
           <div className="col-5 p-1">
@@ -9,6 +22,8 @@ function AddDestination() {
               type="text"
               className="form-control"
               placeholder="Enter city..."
+              value={city}
+              onChange={(e) => setCity(e.target.value)}
             />
           </div>
           <div className="col-5 p-1">
@@ -16,6 +31,8 @@ function AddDestination() {
               type="text"
               className="form-control"
               placeholder="Enter country..."
+              value={country}
+              onChange={(e) => setCountry(e.target.value)}
             />
           </div>
           <div className="col-2 p-1">
